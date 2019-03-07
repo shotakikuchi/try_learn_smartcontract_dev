76bfa6baaa7a3d4da3dd5b183ea7a9bbebc8b165
shotakikuchi <kikuchi@bank.co.jp>
2019-03-07 11:51:20 +0900

    lesson 5 end.

---
+++ crypto_zombies/zombieownership.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.19;
+
+import "./zombieattack.sol.sol";
+import "./erc721.sol";
+import "./safemath.sol";
+
+
+/// @title A contract for controle transfering zombie ownership
+/// @author Shota Kikuchi
+/// @dev This contract is based of OpenZeppelin ERC721 draft Implementation
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    using SafeMath for uint256;
+
+    mapping (uint => address) zombieApprovals;
+
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        return zombieToOwner[_tokenId];
+    }
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+        zombieToOwner[_tokenId] = _to;
+        Transfer(_from, _to, _tokenId);
+    }
+
+    function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        _transfer(msg.sender, _to, _tokenId);
+    }
+
+    function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        zombieApprovals[_tokenId] = _to;
+        Approval(msg.sender, _to, _tokenId);
+    }
+
+    function takeOwnership(uint256 _tokenId) public {
+        require(zombieApprovals[_tokenId] == msg.sender);
+        address owner = ownerOf(_tokenId);
+        _transfer(owner, msg.sender, _tokenId);
+    }
+}
